52724d12aca189f32b4bb6c8bafed2df94e26041
Magdy156 <magdyhamdy156@gmail.com>
2023-08-10 17:57:06 +0300

    task five modified

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """BaseModel Class"""
 from uuid import uuid4
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
@@ -27,6 +27,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ return string representation of the object"""
@@ -37,6 +38,7 @@ class BaseModel:
         """updates the public instance attribute
             updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
